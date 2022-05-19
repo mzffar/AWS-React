1884bbc13bac9e5a2a673387385ab39fdf457cc7
“mzffar” <amazaffar.work@gmail.com>
2022-05-19 23:26:59 +0800

    chg text

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from MEE</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   
       </header>
     </div>
   );
