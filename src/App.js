37660735c499c7ac9b8793a0617d48d204cf2cec
“mzffar” <amazaffar.work@gmail.com>
2022-05-23 10:10:26 +0800

    Text chg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from MEE</h1>
+        <h1>Deploy React to AWS</h1>
    
       </header>
     </div>
