ec36d2341471f725e31ed8df424a41f34a57f705
“mzffar” <amazaffar.work@gmail.com>
2022-05-23 10:39:06 +0800

    aded auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import { Amplify } from 'aws-amplify';
+Amplify.configure(awsExports);
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Deploy React to AWS</h1>
-   
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
-export default App;
+
+
+
